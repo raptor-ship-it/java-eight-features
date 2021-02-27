dc6e3d5f20684a254f9a251dcab29eea70ee702e
kuldipbajwa <kuldip.bajwa@mail.com>
2021-02-27 22:43:47 +0000

    update

--- src/main/java/com/raptor/functional/OptionalTests.java
+++ src/main/java/com/raptor/functional/OptionalTests.java
@@ -2,6 +2,8 @@ package com.raptor.functional;
 
 import com.raptor.model.User;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
@@ -9,6 +11,7 @@ import java.util.Optional;
 public class OptionalTests {
 
     @Test
+    @DisplayName("using_of")
     public void generateNPEWhenOptionalValueIsNull() {
         User user = null;
         Assertions.assertThrows(NullPointerException.class, () -> Optional.of(user));
@@ -17,6 +20,7 @@ public class OptionalTests {
     }
 
     @Test
+    @DisplayName("using_isPresent")
     public void returnFalseWhenOptionalIsEmpty() {
         Optional<User> user = Optional.empty();
         Assertions.assertFalse(user.isPresent());
@@ -25,6 +29,7 @@ public class OptionalTests {
     }
 
     @Test
+    @DisplayName("using_orElse")
     public void userTypeOptionalValueShouldBeReturnedWhenNonEmpty() {
         final User __user = new User();
         Assertions.assertEquals( __user , Optional.of(__user).orElse(new User()));
@@ -38,6 +43,16 @@ public class OptionalTests {
         Assertions.assertFalse(Optional.ofNullable(user).isPresent());
 
         return;
+    }
+
+    @Test
+    public void printUserDetailsIfAndOnlyIfNonEmpty() {
+        final User user = new User();
+        user.setFirstName("kuldip");
+        user.setLastName("bajwa");
+        Optional.of(user).ifPresent((u) -> System.out.print(u));
+        Assertions.assertEquals(user, Optional.of(user).get(), "user not found");
+
     }
 
 }
